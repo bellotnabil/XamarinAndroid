10e3dd76c215950536699133bc615ba8f82c6c69
Nekszer Lopez Espinoza <nekszer@gmail.com>
2016-04-06 19:58:55 -0500

    Video 6
    Picasso Library

---
+++ Video6/Video6/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Square.Picasso;
+
+namespace Video6
+{
+	[Activity (Label = "Video6", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			Picasso.With(this)
+				.Load("https://www.xamarin.com/content/images/pages/index/platform-screenshot@2x.png")
+				.Into(FindViewById<ImageView>(Resource.Id.imageView));
+		}
+	}
+}
+
+
